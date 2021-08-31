7a48b34db1f1a15d653e27e6f79125f7279f597e
Caleb Phillips <csphillips5@gmail.com>
2021-08-31 09:32:10 -0400

    4.2 temp maintain entire app functionality after extraction from index

---
+++ app.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUrl = process.env.MONGODB_URI;
+console.log('Connecting to', mongoUrl);
+
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({})
+    .then((blogs) => {
+      response.json(blogs);
+    });
+});
+
+app.post('/api/blogs', (request, response, next) => {
+  const blog = new Blog(request.body);
+
+  blog.save()
+    .then((savedBlog) => {
+      response.status(201).json(savedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = app;
