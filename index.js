ae376c534cf756f939a812f29cf1169777fc5720
Caleb Phillips <csphillips5@gmail.com>
2021-08-31 08:55:16 -0400

    4.1 app executes with nodemon, connect to MongoDB, GET all & POST work

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const http = require('http');
+require('dotenv').config();
+// const http = require('http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -13,16 +15,27 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist';
+const mongoUrl = process.env.MONGODB_URI;
+console.log('Connecting to', mongoUrl);
+// const mongoUrl = 'mongodb://localhost/bloglist';
 mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true
-});
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
+
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get('/api/blogs', (request, response) => {
   Blog.find({})
@@ -31,16 +44,17 @@ app.get('/api/blogs', (request, response) => {
     });
 });
 
-app.post('api/blogs', (request, response) => {
+app.post('/api/blogs', (request, response, next) => {
   const blog = new Blog(request.body);
 
   blog.save()
-    .then((result) => {
-      response.status(201).json(result);
-    });
+    .then((savedBlog) => {
+      response.status(201).json(savedBlog);
+    })
+    .catch((error) => next(error));
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
