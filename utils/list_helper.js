59c4deb464e4d40a4f90b3af5ccf1ec5fe21e0ab
Caleb Phillips <csphillips5@gmail.com>
2021-08-31 18:50:51 -0400

    4.4 define totalLikes() & write appropriate test suite

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,16 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogList) => {
+  let allLikes = blogList.reduce((likeSum, currentBlog) => {
+    let currentLike = currentBlog.likes;
+    return likeSum + currentLike;
+  }, 0);
+
+  return allLikes;
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
