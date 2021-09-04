b9b738d267483bd9020e18a05cf6146bc4f639c4
Caleb Phillips <csphillips5@gmail.com>
2021-09-04 13:12:54 -0400

    4.12 define & implement errorHandler middleware for ValidationError

--- utils/middleware.js
+++ utils/middleware.js
@@ -7,7 +7,18 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'Unknown endpoint' });
 };
 
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message);
+
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message });
+  }
+
+  next(err);
+};
+
 module.exports = {
   requestLogger,
-  unknownEndpoint
+  unknownEndpoint,
+  errorHandler
 };
