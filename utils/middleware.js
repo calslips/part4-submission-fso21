d08c5249ab2627280dd010061e08d4120942be10
Caleb Phillips <csphillips5@gmail.com>
2021-09-04 21:25:38 -0400

    extend errorHandler middleware, handle CastError with status code 400

--- utils/middleware.js
+++ utils/middleware.js
@@ -10,7 +10,9 @@ const unknownEndpoint = (request, response) => {
 const errorHandler = (err, req, res, next) => {
   console.error(err.message);
 
-  if (err.name === 'ValidationError') {
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'invalid id format' });
+  } else if (err.name === 'ValidationError') {
     return res.status(400).json({ error: err.message });
   }
 
