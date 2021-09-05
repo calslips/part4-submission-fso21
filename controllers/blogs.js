2da2598eb2c64765c2e44519be95eb4e9994082f
Caleb Phillips <csphillips5@gmail.com>
2021-09-04 21:20:56 -0400

    4.13 implement functionality to delete single blog resource

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -18,4 +18,9 @@ blogsRouter.post('/', async (request, response) => {
   //   .catch((error) => next(error));
 });
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
 module.exports = blogsRouter;
