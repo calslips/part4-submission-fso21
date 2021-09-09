0c8291d0628deac581e0ebef5bf2745691cea3d6
Caleb Phillips <csphillips5@gmail.com>
2021-09-09 01:16:37 -0400

    4.16 add requirements to create new user for username & password fields

--- controllers/users.js
+++ controllers/users.js
@@ -10,6 +10,12 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const body = request.body;
 
+  if (!body.password) {
+    return response.status(400).json({ error: 'password missing' });
+  } else if (body.password.length < 3) {
+    return response.status(400).json({ error: 'password is shorter than the minimum allowed length (3)' });
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
