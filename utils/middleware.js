57add4eb26cfe7093b5c3bee08338ab119c30d9a
Caleb Phillips <csphillips5@gmail.com>
2021-09-09 19:39:21 -0400

    4.19 expand errorHandler to handle JsonWebTokenError

--- utils/middleware.js
+++ utils/middleware.js
@@ -14,6 +14,8 @@ const errorHandler = (err, req, res, next) => {
     return res.status(400).send({ error: 'invalid id format' });
   } else if (err.name === 'ValidationError') {
     return res.status(400).json({ error: err.message });
+  } else if (err.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'invalid token' });
   }
 
   next(err);
