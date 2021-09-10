4fc11daf9584d7925116179d288c706355858c61
Caleb Phillips <csphillips5@gmail.com>
2021-09-10 18:57:12 -0400

    4.22 implement middleware to retrieve user& set to req obj for blog POST & DELETE

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,6 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
+const { retrieveUser } = require('../utils/middleware');
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -11,12 +10,10 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', retrieveUser, async (request, response) => {
   const body = request.body;
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-
-  const user = await User.findById(decodedToken.id);
+  const user = request.user;
 
   const blog = new Blog({
     title: body.title,
@@ -33,14 +30,16 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog);
 });
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-
-  const requesterId = decodedToken.id;
+blogsRouter.delete('/:id', retrieveUser, async (request, response) => {
+  const user = request.user;
 
   const blog = await Blog.findById(request.params.id);
-  if (blog.user.toString() === requesterId) {
+
+  if (user && blog.user.toString() === user.id) {
     await Blog.findByIdAndRemove(request.params.id);
+    user.blogs = user.blogs
+      .filter((blogId) => blogId.toString() !== request.params.id);
+    await user.save();
     response.status(204).end();
   } else {
     response.status(401).json({ error: 'invalid user' });
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 const morgan = require('morgan');
 
 morgan.token('body', (req) => JSON.stringify(req.body));
@@ -5,13 +7,19 @@ const requestLogger = morgan(':method :url :status :res[content-length] - :respo
 
 const retrieveToken = (req, res, next) => {
   const auth = req.headers['authorization'];
-  console.log('### auth header:', auth);
+
   if (auth && auth.toLowerCase().startsWith('bearer ')) {
-    console.log('### coded token value:', auth.split(' ')[1]);
     req.token = auth.split(' ')[1];
   } else {
     req.token = null;
   }
+
+  next();
+};
+
+const retrieveUser = async (req, res, next) => {
+  const decodedToken = jwt.verify(req.token, process.env.SECRET);
+  req.user = await User.findById(decodedToken.id);
 
   next();
 };
@@ -38,5 +46,6 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
-  retrieveToken
+  retrieveToken,
+  retrieveUser
 };
